36f556ee6406a51a4fd1cf097b0333628ae736d4
1706619 <pedrombarreiros12@gmail.com>
2023-05-05 10:17:21 +0100

    Criada classe que representa a tabela de graficas, all good, +-

--- app/src/main/java/pt/ipg/graficas/TabelaGraficas.kt
+++ app/src/main/java/pt/ipg/graficas/TabelaGraficas.kt
@@ -7,7 +7,7 @@ import android.provider.BaseColumns
 class TabelaGraficas(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA){
 
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_marcas INTEGER NOT NULL), FOREIGN KEY (id_marcas) REFERENCES ${TabelaMarcas.NOME_TABELA}(${BaseColumns._ID})")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_marcas INTEGER NOT NULL), FOREIGN KEY (id_marcas) REFERENCES ${TabelaMarcas.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT")
     }
 
     companion object{
