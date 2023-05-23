b0da3f8887bfac23888d19925fcc8449334b2f1c
1706619 <pedrombarreiros12@gmail.com>
2023-05-23 09:51:29 +0100

    Implementado o metodo query do content provider

--- app/src/main/java/pt/ipg/graficas/GraficasContentProvider.kt
+++ app/src/main/java/pt/ipg/graficas/GraficasContentProvider.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
 class GraficasContentProvider: ContentProvider() {
     private var bdOpenHelper : BdGraficasOpenHelper? = null
@@ -16,14 +17,41 @@ class GraficasContentProvider: ContentProvider() {
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
-        p4: String?
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        val bd = bdOpenHelper!!.readableDatabase
+        val id = uri.lastPathSegment
+
+        val endereco = uriMatcher().match(uri)
+
+
+        val tabela = when (endereco){
+           URI_MARCAS -> TabelaMarcas(bd)
+           URI_MARCA_ID -> TabelaMarcas(bd)
+           URI_GRAFICAS -> TabelaGraficas(bd)
+           URI_GRAFICA_ID-> TabelaGraficas(bd)
+           else -> null
+        }
+
+        val (selecao, argsSel) = when (endereco){
+            URI_MARCA_ID, URI_GRAFICA_ID-> Pair ("${BaseColumns._ID}=?", arrayOf(id))
+            else -> Pair(selection, selectionArgs)
+        }
+
+
+    return tabela?.consulta(
+        projection as Array<String>,
+        selecao,
+        selectionArgs as Array<String>,
+        null,
+        null,
+        sortOrder)
     }
+
 
     companion object{
         private const val AUTORIDADE = "pt.ipg.graficas"
@@ -32,12 +60,22 @@ class GraficasContentProvider: ContentProvider() {
         const val GRAFICAS = "graficas"
 
         private const val URI_MARCAS = 100
+        private const val URI_MARCA_ID = 101
         private const val URI_GRAFICAS = 200
+        private const val URI_GRAFICA_ID = 201
 
         fun uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTORIDADE, MARCAS, URI_MARCAS)
+            addURI(AUTORIDADE, "$MARCAS/#", URI_MARCA_ID)
             addURI(AUTORIDADE, GRAFICAS, URI_GRAFICAS)
+            addURI(AUTORIDADE, "$GRAFICAS/#", URI_GRAFICA_ID)
         }
+
+        //content://pt.ipg.graficas/marcas -> 100
+        //content://pt.ipg.graficas/marcas/5 -> 100
+        //content://pt.ipg.graficas/marcas/243 -> 100
+        //content://pt.ipg.graficas/categorias -> 100
+
     }
 
 
