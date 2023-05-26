af0fadcc94b72e3bd27012edbb0103c52c098fb5
1706619 <pedrombarreiros12@gmail.com>
2023-05-26 09:54:09 +0100

    Iniciar o recycler view Graficas(adapter e require context)

--- app/src/main/java/pt/ipg/graficas/ListaGraficasFragment.kt
+++ app/src/main/java/pt/ipg/graficas/ListaGraficasFragment.kt
@@ -5,11 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.recyclerview.widget.LinearLayoutManager
+import pt.ipg.graficas.databinding.FragmentListaGraficasBinding
+import pt.ipg.graficas.databinding.FragmentMenuPrincipalBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,24 +15,33 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ListaGraficasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentListaGraficasBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lista_graficas, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapterGraficas = AdapterGraficas()
+        binding.recyclerViewGraficas.adapter = adapterGraficas
+        binding.recyclerViewGraficas.layoutManager = LinearLayoutManager(requireContext())
     }
 
     companion object {
@@ -50,10 +57,7 @@ class ListaGraficasFragment : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ListaGraficasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
             }
     }
 }
