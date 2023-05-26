9f1a89c9c72dd9d817bc4113ac8f205d06e4c63e
1706619 <pedrombarreiros12@gmail.com>
2023-05-26 09:18:16 +0100

    Criado adaptador de listas

---
+++ app/src/main/java/pt/ipg/graficas/AdapterGraficas.kt
@@ -0,0 +1,25 @@
+package pt.ipg.graficas
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterGraficas: RecyclerView.Adapter<AdapterGraficas.ViewHolderGrafica>() {
+    inner class ViewHolderGrafica(itemView: View) : ViewHolder(itemView){
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderGrafica {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderGrafica, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
