371a7ef18218646efecb888fca5f97415dd82c96
Tubias <pedrombarreiros12@gmail.com>
2023-06-07 01:14:42 +0100

    Mostrar as opções alterar e eliminar apenas quando há graficas selecionadas

--- app/src/main/java/pt/ipg/graficas/ListaGraficasFragment.kt
+++ app/src/main/java/pt/ipg/graficas/ListaGraficasFragment.kt
@@ -28,6 +28,16 @@ class ListaGraficasFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> 
     private val binding get() = _binding!!
 
     var graficaSelecionada : Grafica? = null
+        set(value) {
+            field = value
+
+            val mostrarEliminarAlterar = (value != null)
+
+            val activity = activity as MainActivity
+            activity.mostraOpcaoMenu(R.id.action_editar, mostrarEliminarAlterar)
+            activity.mostraOpcaoMenu(R.id.action_eliminar, mostrarEliminarAlterar)
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/pt/ipg/graficas/MainActivity.kt
+++ app/src/main/java/pt/ipg/graficas/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.fragment.app.Fragment
 import pt.ipg.graficas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var menu: Menu
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
@@ -46,6 +46,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(idMenuAtual, menu)
+        this.menu = menu
+
         return true
     }
 
@@ -70,5 +72,9 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    fun mostraOpcaoMenu(idOpcao: Int, mostrar: Boolean) {
+        menu.findItem(idOpcao).setVisible(mostrar)
     }
 }
